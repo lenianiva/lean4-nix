87580f6ee3f849073ab2053f6d1a83d6cf37b551
Leni Aniva <v@leni.sh>
2024-10-25 21:35:54 -0700

    feat: Parameterized overlay

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     lean = {
       url = "github:leanprover/lean4?ref=v4.12.0";
       flake = false;
@@ -13,36 +13,37 @@
   outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
+    flake-parts,
     lean,
     ...
-  } : let
+  } : flake-parts.lib.mkFlake { inherit inputs; } {
+    flake = (import ./overlay.nix) // {
+      templates = {
+        lib = {
+          path = ./templates/lib;
+          description = "Example Lean Project";
+        };
+        default = self.templates.lib;
+      };
+    };
     systems = [
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-linux"
       "aarch64-darwin"
     ];
-    overlay = import ./overlay.nix { inherit lean; };
-  in {
-    templates = {
-      lib = {
-        path = ./templates/lib;
-        description = "Example Lean Project";
-      };
-      default = self.templates.lib;
-    };
-    overlays.default = overlay;
-  } // flake-utils.lib.eachSystem systems (system: let
+    perSystem = { system, pkgs, ... }: let
+      overlay = import ./overlay.nix;
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ overlay ];
+        #overlays = [ (overlay.readRev "dc2533473114eb8656439ff2b9335209784aa640") ];
+        overlays = [ (overlay.readSrc lean) ];
       };
       checks = pkgs.callPackage ./checks.nix {};
-    in
-    {
-      packages = pkgs.lean // {
+    in {
+      packages = {
         inherit (checks) example;
       };
-    });
+    };
+  };
 }
--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,12 @@
-{ lean } : final: pkgs: let
-  lean-packages = pkgs.callPackage ./packages.nix { src = lean; };
+let
+  readSrc = src: final: prev: prev // {
+    lean = prev.callPackage ./packages.nix { inherit src; };
+  };
+  readFromGit = args: readSrc (builtins.fetchGit args);
+  readRev = rev: readFromGit {
+    url = "https://github.com/leanprover/lean4.git";
+    inherit rev;
+  };
 in {
-  lean = lean-packages;
+  inherit readSrc readFromGit readRev;
 }
