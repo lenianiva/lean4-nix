8d789f899388e6e41ceb75f8170647581be4b515
Leni Aniva <v@leni.sh>
2024-11-15 14:37:18 -0800

    chore: Replace tab with space

--- manifests/v4.13.0.nix
+++ manifests/v4.13.0.nix
@@ -130,7 +130,7 @@
 	        LEAN_CC=${stdenv.cc}/bin/cc ${lean-bin-tools-unwrapped}/bin/leanc -shared ${lib.optionalString stdenv.isLinux "-Wl,-Bsymbolic"} \
 	          ${if stdenv.isDarwin
 	            then "-Wl,-force_load,${Init.staticLib}/libInit.a -Wl,-force_load,${Std.staticLib}/libStd.a -Wl,-force_load,${Lean.staticLib}/libLean.a -Wl,-force_load,${leancpp}/lib/lean/libleancpp.a ${leancpp}/lib/libleanrt_initial-exec.a ${leancpp}/lib/temp/libleanshell.a -lc++"
-						  else "-Wl,--whole-archive ${leancpp}/lib/temp/libleanshell.a -lInit -lStd -lLean -lleancpp ${leancpp}/lib/libleanrt_initial-exec.a -Wl,--no-whole-archive -lstdc++"} \
+	           else "-Wl,--whole-archive ${leancpp}/lib/temp/libleanshell.a -lInit -lStd -lLean -lleancpp ${leancpp}/lib/libleanrt_initial-exec.a -Wl,--no-whole-archive -lstdc++"} \
 	          -lm ${stdlibLinkFlags} \
 	          $(${llvmPackages.libllvm.dev}/bin/llvm-config --ldflags --libs) \
 	          -o $out/$libName
