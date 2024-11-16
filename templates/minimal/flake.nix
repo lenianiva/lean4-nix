7faa49d665b548e69018870f639f24ca4f00b925
Yifei Sun <ysun@hey.com>
2024-11-15 21:22:32 -0500

    refactor: clean up templates

--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -4,43 +4,30 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
     lean4-nix.url = "github:lenianiva/lean4-nix";
   };
 
-  outputs =
-    inputs @ { self
-    , nixpkgs
-    , flake-parts
-    , lean4-nix
-    , ...
-    }: flake-parts.lib.mkFlake { inherit inputs; } {
-      flake = { };
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      perSystem = { system, pkgs, ... }:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ (lean4-nix.readToolchainFile ./lean-toolchain) ];
-          };
-          project = pkgs.lean.buildLeanPackage {
-            name = "Example";
-            roots = [ "Main" ];
-            src = pkgs.lib.cleanSource ./.;
-          };
-        in
-        rec {
-          packages = {
-            inherit (project) executable;
-            default = project.executable;
-          };
-          devShells.default = pkgs.mkShell {
-            buildInputs = [ pkgs.lean.lean-all pkgs.lean.lean ];
-          };
-        };
+  outputs = inputs @ { nixpkgs, flake-parts, systems, lean4-nix, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import systems;
+
+    perSystem = { pkgs, system, ... }: {
+      _module.args.pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (lean4-nix.readToolchainFile ./lean-toolchain) ];
+      };
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs.lean; [ lean lean-all ];
+      };
+
+      formatter = pkgs.nixpkgs-fmt;
+
+      packages.default = (pkgs.lean.buildLeanPackage {
+        name = "Example";
+        roots = [ "Main" ];
+        src = pkgs.lib.cleanSource ./.;
+      }).executable;
     };
+  };
 }
