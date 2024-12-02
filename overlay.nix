d516c8669fe805bd0890e64903a930bd5994d80a
Leni Aniva <v@leni.sh>
2024-12-02 09:59:23 -0800

    chore: Add formatter

--- overlay.nix
+++ overlay.nix
@@ -1,27 +1,47 @@
 let
   manifests = import ./manifests;
-  readSrc = { src, bootstrap } : final: prev: prev // rec {
-    lean = (prev.callPackage ./lib/packages.nix { inherit src bootstrap; }) // {
-      lake = lean.Lake-Main.executable;
+  readSrc = {
+    src,
+    bootstrap,
+  }: final: prev:
+    prev
+    // rec {
+      lean =
+        (prev.callPackage ./lib/packages.nix {inherit src bootstrap;})
+        // {
+          lake = lean.Lake-Main.executable;
+        };
     };
-  };
-  readFromGit = { args, bootstrap }: readSrc { src = builtins.fetchGit args; inherit bootstrap; };
-  readRev = { rev, bootstrap, tag }: readFromGit {
-    args = {
-      url = "https://github.com/leanprover/lean4.git";
-      shallow = true;
-      ref = "refs/tags/${tag}";
-      inherit rev;
+  readFromGit = {
+    args,
+    bootstrap,
+  }:
+    readSrc {
+      src = builtins.fetchGit args;
+      inherit bootstrap;
     };
-    inherit bootstrap;
-  };
-  tags = builtins.mapAttrs (tag: manifest: readRev { inherit (manifest) tag rev bootstrap; }) manifests;
-  readToolchain = toolchain : builtins.addErrorContext "Only leanprover/lean4:{tag} toolchains are supported" (let
-    matches = builtins.match "^[[:space:]]*leanprover/lean4:([a-zA-Z0-9\\-\\.]+)[[:space:]]*$" toolchain;
-    tag = builtins.head matches;
-  in
-    builtins.getAttr tag tags);
-  readToolchainFile = toolchainFile : readToolchain (builtins.readFile toolchainFile);
+  readRev = {
+    rev,
+    bootstrap,
+    tag,
+  }:
+    readFromGit {
+      args = {
+        url = "https://github.com/leanprover/lean4.git";
+        shallow = true;
+        ref = "refs/tags/${tag}";
+        inherit rev;
+      };
+      inherit bootstrap;
+    };
+  tags = builtins.mapAttrs (tag: manifest: readRev {inherit (manifest) tag rev bootstrap;}) manifests;
+  readToolchain = toolchain:
+    builtins.addErrorContext "Only leanprover/lean4:{tag} toolchains are supported" (let
+      matches = builtins.match "^[[:space:]]*leanprover/lean4:([a-zA-Z0-9\\-\\.]+)[[:space:]]*$" toolchain;
+      tag = builtins.head matches;
+    in
+      builtins.getAttr tag tags);
+  readToolchainFile = toolchainFile: readToolchain (builtins.readFile toolchainFile);
 in {
   inherit readSrc readFromGit readRev tags readToolchain readToolchainFile;
 }
