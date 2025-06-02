78cb7f2bce0bb0c3e39aa5131d5311d67f9368d5
Leni Aniva <v@leni.sh>
2025-06-02 09:33:53 -0700

    chore: Update manifests

--- templates/dependency/lakefile.lean
+++ templates/dependency/lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 require aesop from git
-  "https://github.com/leanprover-community/aesop.git" @ "v4.19.0"
+  "https://github.com/leanprover-community/aesop.git" @ "v4.20.0"
 
 package Example
 
