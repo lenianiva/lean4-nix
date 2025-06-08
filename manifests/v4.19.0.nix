62ccc8b934765efe22921640c8877bbc9bcb87b0
Leni Aniva <v@leni.sh>
2025-06-08 10:07:56 -0700

    fix: Use nativeBuildInputs

--- manifests/v4.19.0.nix
+++ manifests/v4.19.0.nix
@@ -58,7 +58,7 @@
         name = "lean-src";
         inherit (args0) src;
 
-        builtInputs = [mimalloc-src];
+        nativeBuildInputs = [mimalloc-src];
         patches = [mimalloc-patch];
         postPatch = let
           pattern = "\${LEAN_BINARY_DIR}/../mimalloc/src/mimalloc";
