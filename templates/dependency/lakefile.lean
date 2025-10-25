feb57b6bb14b84849f2565db5708e9694d3388bc
Leni Aniva <a@leni.sh>
2025-10-24 19:43:15 -0700

    chore: Update templates

--- templates/dependency/lakefile.lean
+++ templates/dependency/lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 require aesop from git
-  "https://github.com/leanprover-community/aesop.git" @ "v4.23.0"
+  "https://github.com/leanprover-community/aesop.git" @ "v4.24.0"
 
 package Example
 
