9d11b9e4f922e0d8bc2c0bc16981d31383003e39
Leni Aniva <a@leni.sh>
2026-02-22 10:21:16 -0800

    chore: Add manifest for v4.28.0

---
+++ manifests/v4.28.0.nix
@@ -0,0 +1,22 @@
+{
+  tag = "v4.28.0";
+  rev = "7e01a1bf5c70fc6167d49c345d3bf80596e9a79b";
+  toolchain = {
+    aarch64-linux = {
+      url = "https://github.com/leanprover/lean4/releases/download/v4.28.0/lean-4.28.0-linux_aarch64.tar.zst";
+      hash = "sha256-yGWAEmHHR9TxXQi+ypq8IKypB5BKu7KE3iWjf0tFWLw=";
+    };
+    x86_64-linux = {
+      url = "https://github.com/leanprover/lean4/releases/download/v4.28.0/lean-4.28.0-linux.tar.zst";
+      hash = "sha256-zrOj+ET3rr9jJF4rUcKNWw7TiULBn5PPP+vVIDAhYL0=";
+    };
+    x86_64-darwin = {
+      url = "https://github.com/leanprover/lean4/releases/download/v4.28.0/lean-4.28.0-darwin.tar.zst";
+      hash = "sha256-TJfaEKkm2Qat8z/JmKJUakED6gzfmVzs78G6ox/twAg=";
+    };
+    aarch64-darwin = {
+      url = "https://github.com/leanprover/lean4/releases/download/v4.28.0/lean-4.28.0-darwin_aarch64.tar.zst";
+      hash = "sha256-YZQvnRkH25GAIBVKUXyH+2SEHkjOuwAy/AkJ340YmgU=";
+    };
+  };
+}
