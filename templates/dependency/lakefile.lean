32e5f8082c3d38bd1ab796f82a0346f1ddce1fb2
samuelburnham <45365069+samuelburnham@users.noreply.github.com>
2026-07-20 13:15:29 -0400

    "feat(manifest): Add v4.32.0"

--- templates/dependency/lakefile.lean
+++ templates/dependency/lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 require aesop from git
-  "https://github.com/leanprover-community/aesop.git" @ "v4.31.0"
+  "https://github.com/leanprover-community/aesop.git" @ "v4.32.0"
 
 package Example
 
